b9b24c88242d2fef924384d93933b384605a8e8b
BedrettinMusab <bedorettin4646@gmail.com>
2024-10-19 00:13:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["980256607709589524", ""], 
+  ownerID: ["1004206704994566164", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
